a8079f467cf25b9af299008124194777a95f105f
Jean7721 <jsaavedray@ing.ucsc.cl>
2022-09-26 16:57:47 -0300

    Reconstrucción

--- n_reinas.py
+++ n_reinas.py
@@ -7,7 +7,9 @@ if len(sys.argv) == 4 :
     semilla = int(sys.argv[1])
     tamanoTableros = int(sys.argv[2])
     cantidadTableros = int(sys.argv[3])
-    print("semilla: ", semilla, " Tamaño del tablero: ", tamanoTableros, " Cantidad de tableros: ", cantidadTableros)
+    print("")
+    print("Semilla: ", semilla, " Tamaño del tablero: ", tamanoTableros, " Cantidad de tableros: ", cantidadTableros)
+    print("")
 else:
     print("Porfavor reingrese los parámetros de manera correcta.")
     print("Parametros a ingresar: 'Nombre del archivo' 'Semilla' 'Tamaño de tablero' 'Cantidad de tableros'")
@@ -21,7 +23,9 @@ for i in range(cantidadTableros):
     poblacionInicial[i] = np.arange(0, tamanoTableros)
     np.random.shuffle(poblacionInicial[i])
 
+print("La población inicial es: ")
 print(poblacionInicial)
+print("")
 
 maximoFitness = tamanoTableros*(tamanoTableros-1)/2
 choquesDiagonales = np.zeros((cantidadTableros), float)
@@ -32,21 +36,18 @@ for tablero in range(len(poblacionInicial)):
         for j in range(len(poblacionInicial[tablero])):
             if abs(i-j) == abs(poblacionInicial[tablero][i] - poblacionInicial[tablero][j]) and i != j:
                 choquesDiagonales[tablero] += 1
-                choquesDiagonales[tablero] /= 2
                 """ print("Tablero N°: ",tablero ,
                 " Donde fila", i, " Valor ", poblacionInicial[tablero][i],
                 " Donde fila", j, " Valor ", poblacionInicial[tablero][j],
                 "La resta de iteradores es: ", abs(i-j),
                 "La resta de resultados es: ", abs(poblacionInicial[tablero][i] - poblacionInicial[tablero][j])) """
+    choquesDiagonales[tablero] /= 2
 
+print("El arreglo de choques: ")
+print(choquesDiagonales)
+print("")
 suma_total = 0
 suma_total = np.sum(choquesDiagonales)
 
-for i in range(len(choquesDiagonales)):
-    choquesDiagonales[i] = maximoFitness - choquesDiagonales[i]
-    choquesDiagonales[i] /= suma_total
-    choquesDiagonales[i] = round(choquesDiagonales[i],2)
-    
-
-
-print(choquesDiagonales)
+print("La suma total del arreglo de choques es: ")
+print(suma_total)
